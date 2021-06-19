54bfd3befbaea269a6d6b164e33b55b0f0cc6de6
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-19 17:46:43 -0400

    commit tree 49f543da07e834b381ffeb54c28cf2a443b45924

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+    const hq = 42;
+    
+    if (block > 42) {
+        return block - hq;
+    }
+    else if (block < 42) {
+        return hq - block;
+    };
+
+};
+
+function distanceTravelledInFeet(block1, block2) {
+
+};
